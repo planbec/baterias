a2d259c509d547ae7b4a094eb0fa9af15abcc08a
planbec <carlosz@planb.ec>
2017-09-12 09:45:14 -0500

    Colores en header y modificaciones en portada

--- home-productos.php
+++ home-productos.php
@@ -3,30 +3,34 @@
 			<h1><span>Nuestros productos</span></h1>
 			<div class="cols cf">
 				
-				<article class="servicio-resumen col2">
-					<div>
-						<a href="#"><img src="img/tn_calidad.png"></a>
-					</div><!-- /.icono -->
-					
-					<div class="text">
-						<h2>Calidad de vida</h2>
-						<p>Ofrecemos la mayor probabilidad de curar muchos tipos de c&aacute;ncer y prevenci&oacute;n de su propagaci&oacute;n...</p>
+				<article>
+					<div class="producto">
+						<a href="#"><img src="img/foto_aura.jpg"></a>
 						<a href="calidad.php">Ver m&aacute;s</a>
-					</div><!-- /.text -->
-				</article><!-- /.servicio-resumen -->
-				
-				
-				<article class="servicio-resumen col2">
-					<div>
-						<a href="#"><img src="img/tn_quimio.png"></a>
 					</div><!-- /.icono -->
+
+					<div class="producto">
+						<a href="#"><img src="img/foto_aura.jpg"></a>
+						<a href="calidad.php">Ver m&aacute;s</a>
+					</div><!-- /.icono -->
+
+					<div class="producto">
+						<a href="#"><img src="img/foto_aura.jpg"></a>
+						<a href="calidad.php">Ver m&aacute;s</a>
+					</div><!-- /.icono -->
+
+					<div class="producto">
+						<a href="#"><img src="img/foto_aura.jpg"></a>
+						<a href="calidad.php">Ver m&aacute;s</a>
+					</div><!-- /.icono -->
+
+					<div class="producto">
+						<a href="#"><img src="img/foto_aura.jpg"></a>
+						<a href="calidad.php">Ver m&aacute;s</a>
+					</div><!-- /.icono -->
+
+					<a href="#" class="call-to-action">Ver más productos</a>
 					
-					<div class="text">
-						<h2>Tratamiento Quimioter&aacute;pia</h2>
-						<p>Conozca sobre el uso de f&aacute;rmacos para destruir las c&eacute;lulas cancerosas...</p>
-						<a href="quimioterapia.php">Ver m&aacute;s</a>
-					</div><!-- /.text -->
-				</article><!-- /.servicio-resumen -->
 				
 			</div><!-- /.cols -->
 			
